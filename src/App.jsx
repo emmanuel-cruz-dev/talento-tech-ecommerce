a42e6d05c8a08d282f33ce99dec12f8fc07b9210
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-01 08:05:27 -0300

    feat: adds NavigationBar and Footer to src/App.jsx, and removes App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./routes/AppRouter";
+import NavigationBar from "./layouts/NavigationBar";
+import Footer from "./layouts/Footer";
 
 function App() {
   return (
     <Router>
-      <AppRouter />
+      <div className="d-flex flex-column min-vh-100">
+        <NavigationBar />
+        <main className="flex-grow-1">
+          <AppRouter />
+        </main>
+        <Footer />
+      </div>
     </Router>
   );
 }
