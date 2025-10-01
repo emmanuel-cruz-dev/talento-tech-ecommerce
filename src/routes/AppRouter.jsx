3896cabac1a6b82fc76621d338c33cccf3ff8667
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-01 08:09:16 -0300

    feat: adds Cart.jsx to src/routes/AppRouter.jsx

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -3,16 +3,18 @@ import Home from "../pages/Home.jsx";
 import Products from "../pages/Products.jsx";
 import Product from "../pages/Product.jsx";
 import NotFound from "../pages/NotFound.jsx";
+import Cart from "../pages/Cart.jsx";
 
-const AppRouter = () => {
+function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/products" element={<Products />} />
       <Route path="/product/:id" element={<Product />} />
+      <Route path="/cart" element={<Cart />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
-};
+}
 
 export default AppRouter;
