6c8786a4be8a2691932795a8230adb042c31b916
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-01 12:58:27 -0300

    feat: adds Hero, Benefits, Categories, Testimonials and Newsletter to src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,51 +1,22 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
+import Hero from "../components/ui/Hero";
+import ProductList from "../components/ui/product/ProductList";
+import Benefits from "../components/ui/Benefits";
+import Categories from "../components/ui/Categories";
+import Testimonials from "../components/ui/Testimonials";
+import Newsletter from "../components/ui/Newsletter";
 
-const Home = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  const handleLoad = () => {
-    setIsLoading(false);
-  };
-
-  window.addEventListener("load", handleLoad);
-
+function Home() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12 text-center">
-          <h1>Tech E-Commerce</h1>
-          <p className="lead">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            elementum, nulla sit amet elementum molestie, nisl risus tincidunt
-            nisl, sit amet ullamcorper risus nisl id nisl. Sed elementum, nulla
-            sit amet elementum molestie, nisl risus tincidunt nisl, sit amet
-            ullamcorper risus nisl id nisl. Sed elementum, nulla sit amet
-            elementum molestie, nisl risus tincidunt nisl, sit amet ullamcorper
-            risus nisl id nisl. Sed elementum, nulla sit amet elementum
-            molestie, nisl risus tincidunt nisl, sit amet ullamcorper risus nisl
-            id nisl. Sed elementum, nulla sit amet elementum molestie, nisl
-            risus tincidunt nisl, sit amet ullamcorper risus nisl id nisl.
-          </p>
-        </div>
-      </div>
-      {isLoading ? (
-        <div className="row">
-          <div className="col-12 text-center">
-            <p>Loading...</p>
-          </div>
-        </div>
-      ) : (
-        <div className="row">
-          <div className="col-12 text-center">
-            <Link to="/products" className="btn btn-primary">
-              View Products
-            </Link>
-          </div>
-        </div>
-      )}
-    </div>
+    <>
+      <Hero />
+      <Benefits />
+      <ProductList />
+      <Categories />
+      <Testimonials />
+      <Newsletter />
+    </>
   );
-};
+}
 
 export default Home;
