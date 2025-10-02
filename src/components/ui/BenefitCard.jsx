9c160e271661250acec5ebfee589dff93b4e5fc8
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-02 10:23:19 -0300

    feat: adds BenefitCard.jsx to src/components/ui/

---
+++ src/components/ui/BenefitCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Col } from "react-bootstrap";
+
+const BenefitCard = ({ title, description, icon: Icon }) => {
+  return (
+    <Col md={4} className="mb-4 mb-md-0">
+      <Icon size={40} className="text-primary mb-3" />
+      <h5 className="fw-bold">{title}</h5>
+      <p className="text-muted">{description}</p>
+    </Col>
+  );
+};
+
+export default BenefitCard;
