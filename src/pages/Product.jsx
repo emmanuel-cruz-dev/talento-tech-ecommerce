e90b11c6639eb70812ba3a385cf6130406cf1550
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-02 08:47:21 -0300

    feat: adds Product.jsx to src/pages/

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useProductById } from "../hooks/useProducts";
 
-const Product = () => {
+function Product() {
+  const { id } = useParams();
+  const { product, loading, error } = useProductById(id);
+
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <section>
       <h1>Product</h1>
+      <p>Product ID: {product.id}</p>
+      <p>Product Name: {product.name}</p>
+      <p>Product Description: {product.description}</p>
+      <p>Product Price: {product.price}</p>
+      <img src={product.image} alt={product.name} />
     </section>
   );
-};
+}
 
 export default Product;
