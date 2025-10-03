cd71f36cf185bb7fde3de3c2f29a015de6fb9b27
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-03 11:23:05 -0300

    feat: adds useProductCard and updates structure in src/components/ui/product/ProductCard.jsx

--- src/components/ui/product/ProductCard.jsx
+++ src/components/ui/product/ProductCard.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { Card, Button } from "react-bootstrap";
 import ProductCardPlaceholder from "./ProductCardPlaceholder";
+import { useProductCard } from "../../../hooks/useProductCard";
 
 function ProductCard({
   id,
@@ -11,23 +12,35 @@ function ProductCard({
   image,
   isLoading = false,
 }) {
+  const { handleAddToCartClick } = useProductCard({
+    id,
+    title,
+    price,
+    description,
+    image,
+  });
+
   if (isLoading) return <ProductCardPlaceholder />;
 
   return (
     <Card>
-      <Card.Img
-        className="py-2"
-        variant="top"
-        src={image}
-        alt={title}
-        style={{
-          height: "15rem",
-          objectFit: "contain",
-          backgroundColor: "#e9ecef",
-        }}
-      />
+      <Link to={`/product/${id}`} className="text-decoration-none">
+        <Card.Img
+          className="py-2"
+          variant="top"
+          src={image}
+          alt={title}
+          style={{
+            height: "15rem",
+            objectFit: "contain",
+            backgroundColor: "#e9ecef",
+          }}
+        />
+      </Link>
       <Card.Body>
-        <Card.Title className="line-clamp-1">{title}</Card.Title>
+        <Link to={`/product/${id}`} className="text-decoration-none text-dark">
+          <Card.Title className="line-clamp-1">{title}</Card.Title>
+        </Link>
         <Card.Text
           className="mb-1"
           style={{ color: "blue", fontSize: "1.2rem", fontWeight: "500" }}
@@ -40,9 +53,13 @@ function ProductCard({
         >
           {description}
         </Card.Text>
-        <Link to={`/product/${id}`} className="btn btn-primary">
-          Ver más
-        </Link>
+        <Button
+          variant="primary"
+          className="mt-3 d-inline-flex align-items-center gap-2 px-4"
+          onClick={handleAddToCartClick}
+        >
+          Añadir al carrito
+        </Button>
       </Card.Body>
     </Card>
   );
