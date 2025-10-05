d02d16f54e4dbed705f98a4bcfa8f4deaad7b4e7
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-05 12:55:32 -0300

    feat: adds ProductDetailsCard and ProductSlideList to src/pages/Product.jsx

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,26 +1,23 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useProductById } from "../hooks/useProducts";
+import ProductSlideList from "../components/ui/product/ProductSlideList";
+import ProductDetailsCard from "../components/ui/product/ProductDetailsCard";
 
 function Product() {
   const { id } = useParams();
   const { product, loading, error } = useProductById(id);
 
-  if (loading) {
-    return <div>Cargando...</div>;
-  }
   if (error) {
     return <div>Error: {error}</div>;
   }
 
   return (
     <section>
-      <h1>Product</h1>
-      <p>Product ID: {product.id}</p>
-      <p>Product Name: {product.name}</p>
-      <p>Product Description: {product.description}</p>
-      <p>Product Price: {product.price}</p>
-      <img src={product.image} alt={product.name} />
+      <ProductDetailsCard product={product} isLoading={loading} />
+      <article>
+        <ProductSlideList title="Productos Destacados" />
+      </article>
     </section>
   );
 }
