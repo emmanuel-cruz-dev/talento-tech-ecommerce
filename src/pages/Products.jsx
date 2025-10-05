d73043943f22a7d71a63d8ab23235ff4be3a366a
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-05 12:58:32 -0300

    feat: adds ProductsGallery to src/pages/Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,21 +3,15 @@ import ProductsGallery from "../components/ui/product/ProductsGallery";
 
 function Products() {
   return (
-    <>
-      <div className="d-flex flex-column flex-sm-row gap-3 justify-content-center ">
-        <aside className="pt-4 px-4 bg-light">
-          <h2>Categorías</h2>
-          <ul className="list-unstyled">
-            <li>Electrónica</li>
-            <li>Ropa</li>
-            <li>Hogar</li>
-            <li>Accesorios</li>
-          </ul>
-        </aside>
+    <section className="d-flex flex-column flex-sm-row gap-3 justify-content-center mb-5">
+      <aside className="pt-4 px-4 bg-white">
+        <h2>Categorías</h2>
+        {/* TODO: Agregar filtros por categoría */}
+        {/* TODO: Ordenar por precio */}
+      </aside>
 
-        <ProductsGallery />
-      </div>
-    </>
+      <ProductsGallery />
+    </section>
   );
 }
 
