c08b87e5f54af7cdcc78e6565b747c18691e2e44
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-05 12:57:42 -0300

    feat: adds useProductCard to src/components/ui/product/ProductInfoCard.jsx

--- src/components/ui/product/ProductInfoCard.jsx
+++ src/components/ui/product/ProductInfoCard.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import { Card, Row, Col, Button, Badge } from "react-bootstrap";
 import { Star, ShoppingCart, Heart } from "lucide-react";
-import { useCart } from "../../../context/CartContext";
 import { capitalizeFirstLetter } from "../../../utils/utils";
+import { useProductCard } from "../../../hooks/useProductCard";
 
 const ProductInfoCard = ({ product }) => {
-  const { handleAddToCart } = useCart();
+  const { handleAddToCartClick } = useProductCard({
+    id: product.id,
+    title: product.title,
+    price: product.price,
+    description: product.description,
+    image: product.image,
+  });
 
   return (
     <Card className="shadow-sm border-0 mb-3">
       <Card.Body>
-        <Badge bg="secondary" className="mb-2 p-2">
+        <Badge bg="secondary" className="my-2 p-2">
           {product.category}
         </Badge>
-        <h1 className="display-6 fw-bold mb-3">{product.title}</h1>
+        <h2 className="fw-bold mb-3">{product.title}</h2>
         <div className="d-flex align-items-center mb-3">
           <div className="d-flex me-2">
             {[...Array(5)].map((_, index) => (
@@ -28,7 +34,7 @@ const ProductInfoCard = ({ product }) => {
         </div>
 
         <div className="mb-4">
-          <span className="display-4 fw-bold text-primary">
+          <span className="display-6 fw-bold text-primary">
             ${product.price}
           </span>
         </div>
@@ -49,20 +55,19 @@ const ProductInfoCard = ({ product }) => {
           )}
         </div>
 
-        <Row className="g-3 mb-4">
+        <Row className="g-3 mb-3">
           <Col xs={12} md={8}>
             <Button
               variant="primary"
-              size="lg"
-              className="w-100"
-              onClick={handleAddToCart}
+              className="w-100 py-2"
+              onClick={handleAddToCartClick}
             >
               <ShoppingCart size={20} className="me-2" />
               Agregar al carrito
             </Button>
           </Col>
           <Col xs={12} md={4}>
-            <Button variant="outline-secondary" size="lg" className="w-100">
+            <Button variant="outline-secondary" className="w-100 py-2">
               <Heart size={20} />
             </Button>
           </Col>
