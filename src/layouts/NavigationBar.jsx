7e5f1372ef61208bb131f2304eaa913e4f6c9a45
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-08 16:33:30 -0300

    feat: adds useAuth and useNavigate to src/layouts/NavigationBar.jsx

--- src/layouts/NavigationBar.jsx
+++ src/layouts/NavigationBar.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { useCart } from "../context/CartContext";
+import { Link, useNavigate } from "react-router-dom";
 import { ShoppingCart } from "lucide-react";
+import { useCart } from "../context/CartContext";
+import { useAuth } from "../context/AuthContext";
 
 function NavigationBar() {
   const { getCartItemsCount } = useCart();
+  const { isAuthenticated, login, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+  };
 
   return (
     <Navbar bg="black" variant="dark" expand="lg" sticky="top">
@@ -32,12 +40,22 @@ function NavigationBar() {
             <Nav.Link as={Link} to="/products">
               Productos
             </Nav.Link>
-            <Nav.Link as={Link} to="/cart" className="cart__items-container">
-              <ShoppingCart size={20} className="me-2" />
-              {getCartItemsCount() > 0 && (
-                <span className="cart__items-badge">{getCartItemsCount()}</span>
-              )}
+            <Nav.Link
+              onClick={isAuthenticated ? handleLogout : login}
+              className="border-0 bg-transparent tex"
+            >
+              {isAuthenticated ? "Cerrar sesión" : "Iniciar sesión"}
             </Nav.Link>
+            {isAuthenticated && (
+              <Nav.Link as={Link} to="/cart" className="cart__items-container">
+                <ShoppingCart size={20} className="me-2" />
+                {getCartItemsCount() > 0 && (
+                  <span className="cart__items-badge">
+                    {getCartItemsCount()}
+                  </span>
+                )}
+              </Nav.Link>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
