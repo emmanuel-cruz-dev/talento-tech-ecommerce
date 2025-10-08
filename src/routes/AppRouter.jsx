0936c963d335fd238577e920ec0589c25aac1fef
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-08 16:31:11 -0300

    feat: add Login, PrivateRoute and Profile to src/routes/AppRouter.jsx

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -4,14 +4,33 @@ import Product from "../pages/Product.jsx";
 import NotFound from "../pages/NotFound.jsx";
 import Cart from "../pages/Cart.jsx";
 import Products from "../pages/Products.jsx";
+import PrivateRoute from "./PrivateRoute.jsx";
+import Profile from "../pages/Profile.jsx";
+import Login from "../pages/Login.jsx";
 
 function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/products/" element={<Products />} />
       <Route path="/product/:id" element={<Product />} />
-      <Route path="/cart" element={<Cart />} />
+      <Route
+        path="/cart"
+        element={
+          <PrivateRoute>
+            <Cart />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/profile"
+        element={
+          <PrivateRoute>
+            <Profile />
+          </PrivateRoute>
+        }
+      />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
