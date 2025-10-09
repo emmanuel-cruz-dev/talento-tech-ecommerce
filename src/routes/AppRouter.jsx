1d1a588ae24901931822c7ab914309c1df424c85
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-09 17:33:29 -0300

    feat: adds and implements Suspense and lazy in src/routes/AppRouter.jsx

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,38 +1,43 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home.jsx";
-import Product from "../pages/Product.jsx";
-import NotFound from "../pages/NotFound.jsx";
-import Cart from "../pages/Cart.jsx";
-import Products from "../pages/Products.jsx";
-import PrivateRoute from "./PrivateRoute.jsx";
-import Profile from "../pages/Profile.jsx";
-import Login from "../pages/Login.jsx";
+import { Suspense, lazy } from "react";
+import { Loader } from "lucide-react";
+
+const Home = lazy(() => import("../pages/Home.jsx"));
+const Product = lazy(() => import("../pages/Product.jsx"));
+const Cart = lazy(() => import("../pages/Cart.jsx"));
+const Products = lazy(() => import("../pages/Products.jsx"));
+const Profile = lazy(() => import("../pages/Profile.jsx"));
+const Login = lazy(() => import("../pages/Login.jsx"));
+const NotFound = lazy(() => import("../pages/NotFound.jsx"));
+const PrivateRoute = lazy(() => import("./PrivateRoute.jsx"));
 
 function AppRouter() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/products/" element={<Products />} />
-      <Route path="/product/:id" element={<Product />} />
-      <Route
-        path="/cart"
-        element={
-          <PrivateRoute>
-            <Cart />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/profile"
-        element={
-          <PrivateRoute>
-            <Profile />
-          </PrivateRoute>
-        }
-      />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/products/" element={<Products />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route
+          path="/cart"
+          element={
+            <PrivateRoute>
+              <Cart />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
   );
 }
 
