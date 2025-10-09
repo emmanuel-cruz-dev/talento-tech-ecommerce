0d7d78b09120515564f46f7ee05d448af012e247
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-09 16:35:21 -0300

    feat: adds getProductsByCategory to  src/services/product.js

--- src/services/product.js
+++ src/services/product.js
@@ -32,7 +32,29 @@ const getProductById = async (id) => {
   }
 };
 
+const getProductsByCategory = async (category) => {
+  try {
+    const url =
+      category === "all"
+        ? `${BASE_URL}/products`
+        : `${BASE_URL}/products/category/${category}`;
+
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Error fetching products: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
 export const productService = {
   getProducts,
   getProductById,
+  getProductsByCategory,
 };
