becc7ede2113cc0fb5e9267b13ecf189edcfcba0
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-10 10:34:08 -0300

    feat: adds handle user logic to src/context/AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -6,21 +6,35 @@ export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(() => {
     return localStorage.getItem("isLoggedIn") === "true";
   });
+  const [user, setUser] = useState(() => {
+    const savedUser = localStorage.getItem("userName");
+    return savedUser || null;
+  });
 
   useEffect(() => {
     localStorage.setItem("isLoggedIn", isAuthenticated);
   }, [isAuthenticated]);
 
-  const login = () => {
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem("userName", user);
+    } else {
+      localStorage.removeItem("userName");
+    }
+  }, [user]);
+
+  const login = (userName) => {
     setIsAuthenticated(true);
+    setUser(userName);
   };
 
   const logout = () => {
     setIsAuthenticated(false);
+    setUser(null);
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+    <AuthContext.Provider value={{ isAuthenticated, user, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
