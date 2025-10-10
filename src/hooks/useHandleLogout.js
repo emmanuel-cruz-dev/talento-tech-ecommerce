2ee9b5c47df399ba432be33a075fad1678b3f931
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-10 14:51:11 -0300

    feat: adds and implements handleClearCart in src/hooks/useHandleLogout.js

--- src/hooks/useHandleLogout.js
+++ src/hooks/useHandleLogout.js
@@ -1,11 +1,14 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import { useCart } from "../context/CartContext";
 
 export const useHandleLogout = () => {
   const navigate = useNavigate();
   const { logout } = useAuth();
+  const { handleClearCart } = useCart();
 
   const handleLogout = () => {
+    handleClearCart();
     logout();
     navigate("/");
   };
