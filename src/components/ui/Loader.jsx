71d8ad233374fe70c809b8624fe4b6184930f689
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-10 09:02:35 -0300

    feat: updates styles and structure in src/components/ui/Loader.jsx

--- src/components/ui/Loader.jsx
+++ src/components/ui/Loader.jsx
@@ -1,13 +1,29 @@
-import React from "react";
-import { Spinner } from "react-bootstrap";
+import ShopZoneLogo from "../../assets/shop-zone-banner.avif";
+import { pulseKeyframes, pulseStyle } from "../../constants/pulseStyles";
 
 function Loader() {
   return (
-    <div className="d-flex justify-content-center">
-      <Spinner animation="border" role="status">
-        <span className="visually-hidden">Loading...</span>
-      </Spinner>
-    </div>
+    <>
+      <style>{pulseKeyframes}</style>
+      <section
+        className="d-flex flex-column align-items-center justify-content-center"
+        style={{ height: "80vh" }}
+      >
+        <article className="text-center">
+          <figure style={{ maxWidth: "220px" }}>
+            <img
+              className="img-fluid"
+              src={ShopZoneLogo}
+              alt="Logo de Dragon Ball"
+              width="760"
+              height="760"
+              style={pulseStyle}
+            />
+          </figure>
+          <p>Cargando...</p>
+        </article>
+      </section>
+    </>
   );
 }
 
