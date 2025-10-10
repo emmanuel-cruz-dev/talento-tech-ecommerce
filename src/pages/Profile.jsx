9c9469fb4ce50b3cd3721cb583be2c6e7b1eac59
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-10 10:46:00 -0300

    feat: updates styles and structure in src/pages/Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,23 +1,59 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Container, Button } from "react-bootstrap";
+import { Container, Button, Card } from "react-bootstrap";
 import { useAuth } from "../context/AuthContext";
+import { CircleUserRound } from "lucide-react";
+import { useHandleLogout } from "../hooks/useHandleLogout";
 
 function Profile() {
   const navigate = useNavigate();
-  const { logout } = useAuth();
-
-  const handleLogout = () => {
-    logout();
-    navigate("/");
-  };
+  const { user } = useAuth();
+  const handleLogout = useHandleLogout();
 
   return (
-    <Container className="mt-5">
-      <h1>Bienvenido usuario</h1>
-      <Button variant="primary" onClick={handleLogout}>
-        Cerrar sesión
-      </Button>
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: "90vh" }}
+    >
+      <Card
+        className="shadow-lg border-0 text-center p-4"
+        style={{
+          maxWidth: "440px",
+          width: "100%",
+        }}
+      >
+        <Card.Body>
+          <header className="mb-3">
+            <CircleUserRound size={64} className="text-primary mb-3" />
+            <h2
+              className="fw-bold mb-2"
+              style={{ textTransform: "capitalize" }}
+            >
+              Hola, {user}!
+            </h2>
+            <p className="text-muted">Bienvenido a tu perfil personal</p>
+          </header>
+
+          <hr className="my-4 w-50" style={{ margin: "auto" }} />
+
+          <footer className="d-flex flex-column gap-3">
+            <Button
+              variant="outline-primary"
+              onClick={() => navigate("/")}
+              className="fw-semibold "
+            >
+              Ir al inicio
+            </Button>
+            <Button
+              variant="danger"
+              onClick={handleLogout}
+              className="fw-semibold"
+            >
+              Cerrar sesión
+            </Button>
+          </footer>
+        </Card.Body>
+      </Card>
     </Container>
   );
 }
