e598054966ec81d2676d10773bcd5e32f215f5ce
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-13 07:31:08 -0300

    feat: adds ErrorMessage and handleRetry to src/pages/Product.jsx

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,18 +3,20 @@ import { useParams } from "react-router-dom";
 import { useProductById } from "../hooks/useProducts";
 import ProductSlideList from "../components/ui/product/ProductSlideList";
 import ProductDetailsCard from "../components/ui/product/ProductDetailsCard";
+import ErrorMessage from "../components/ui/ErrorMessage";
+import { handleRetry } from "../utils/utils";
 
 function Product() {
   const { id } = useParams();
   const { product, loading, error } = useProductById(id);
 
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
   return (
     <section>
-      <ProductDetailsCard product={product} isLoading={loading} />
+      {error ? (
+        <ErrorMessage error={error} entity="Producto" onRetry={handleRetry} />
+      ) : (
+        <ProductDetailsCard product={product} isLoading={loading} />
+      )}
       <article>
         <ProductSlideList title="Productos Destacados" />
       </article>
