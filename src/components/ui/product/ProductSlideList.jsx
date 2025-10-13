ea60c62a4c068a19de253982c999220fa8404201
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-13 07:33:52 -0300

    feat: adds ErrorMessage and handleRetry to src/components/ui/product/ProductSlideList.jsx

--- src/components/ui/product/ProductSlideList.jsx
+++ src/components/ui/product/ProductSlideList.jsx
@@ -5,6 +5,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import { useProducts } from "../../../hooks/useProducts";
 import ProductCard from "./ProductCard";
+import ErrorMessage from "../ErrorMessage";
+import { handleRetry } from "../../../utils/utils";
 
 import "swiper/css";
 import "swiper/css/navigation";
@@ -12,8 +14,6 @@ import "swiper/css/pagination";
 
 function ProductSlideList({ title }) {
   const { products, loading, error } = useProducts();
-
-  if (error) return <p>Error al cargar los productos: {error}</p>;
 
   return (
     <Container
@@ -49,33 +49,45 @@ function ProductSlideList({ title }) {
         }}
         style={{ paddingBottom: "3.5rem" }}
       >
-        {loading
-          ? Array.from({ length: 6 }).map((_, index) => (
-              <SwiperSlide key={`placeholder-${index}`}>
-                <ProductCard isLoading={true} />
-              </SwiperSlide>
-            ))
-          : products.map((product, index) => (
-              <SwiperSlide key={`${product.id}-${index}`}>
-                <ProductCard
-                  id={product.id}
-                  title={product.title}
-                  price={product.price}
-                  description={product.description}
-                  category={product.category}
-                  image={product.image}
-                />
-              </SwiperSlide>
-            ))}
+        {loading ? (
+          Array.from({ length: 6 }).map((_, index) => (
+            <SwiperSlide key={`placeholder-${index}`}>
+              <ProductCard isLoading={true} />
+            </SwiperSlide>
+          ))
+        ) : error ? (
+          <Container className="d-flex justify-content-center fs-5">
+            <ErrorMessage
+              error={error}
+              entity="Productos"
+              onRetry={handleRetry}
+            />
+          </Container>
+        ) : (
+          products.map((product, index) => (
+            <SwiperSlide key={`${product.id}-${index}`}>
+              <ProductCard
+                id={product.id}
+                title={product.title}
+                price={product.price}
+                description={product.description}
+                category={product.category}
+                image={product.image}
+              />
+            </SwiperSlide>
+          ))
+        )}
       </Swiper>
 
-      <footer className="d-flex justify-content-center">
-        <Link to="/products">
-          <Button variant="primary" className="mt-4 px-4">
-            Ver más productos
-          </Button>
-        </Link>
-      </footer>
+      {!error && (
+        <footer className="d-flex justify-content-center">
+          <Link to="/products">
+            <Button variant="primary" className="mt-4 px-4">
+              Ver más productos
+            </Button>
+          </Link>
+        </footer>
+      )}
     </Container>
   );
 }
