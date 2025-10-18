26d1dcb438edaff8e55f56e738bb604f471e8a11
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-18 15:13:08 -0300

    feat: adds useQueryHandler.js to src/hooks/

---
+++ src/hooks/useQueryHandler.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import { useCategorySelect } from "./useCategorySelect";
+
+export const useQueryHandler = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const categoryParam = searchParams.get("category") || "all";
+
+  const { selectedCategory, handleCategorySelect } =
+    useCategorySelect(categoryParam);
+
+  useEffect(() => {
+    if (categoryParam !== selectedCategory) {
+      handleCategorySelect(categoryParam)();
+    }
+  }, [categoryParam]);
+
+  const handleCategoryClick = (categoryId) => {
+    handleCategorySelect(categoryId)();
+    setSearchParams({ category: categoryId });
+  };
+
+  return { selectedCategory, handleCategoryClick };
+};
