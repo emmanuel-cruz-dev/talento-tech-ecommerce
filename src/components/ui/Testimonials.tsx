bcd9e231b0b3b76b81308a912bed8b12fa656fd7
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-20 13:57:01 -0300

    chore: convert files .jsx to .tsx in src/components/ui/

--- src/components/ui/Testimonials.tsx
+++ src/components/ui/Testimonials.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { testimonials } from "../../data/testimonials";
 import TestimonialCard from "./TestimonialCard";
