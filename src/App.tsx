425606736a4a30e1f48f0bcc4ac6894f270b54f8
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-20 14:05:31 -0300

    chore: convert files .jsx to .ts inx src/

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRouter from "./routes/AppRouter";
+import NavigationBar from "./layouts/NavigationBar";
+import Footer from "./layouts/Footer";
+import ScrollToTop from "./helpers/ScrollToTop";
+import { CartProvider } from "./context/CartContext";
+import ProgressBar from "./components/ui/ProgressBar";
+
+function App() {
+  return (
+    <Router>
+      <CartProvider>
+        <ProgressBar />
+        <div className="d-flex flex-column min-vh-100">
+          <NavigationBar />
+          <main className="flex-grow-1 bg-light">
+            <AppRouter />
+          </main>
+          <Footer />
+        </div>
+      </CartProvider>
+      <ScrollToTop />
+    </Router>
+  );
+}
+
+export default App;
