c17fe4c6b2373c704d10a6e03d51fe16f132f759
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-22 10:18:07 -0300

    feat: adds types to src/hooks/useCategorySelect.ts

--- src/hooks/useCategorySelect.ts
+++ src/hooks/useCategorySelect.ts
@@ -1,9 +1,9 @@
 import { useState } from "react";
 
-export const useCategorySelect = (category) => {
+export const useCategorySelect = (category: string) => {
   const [selectedCategory, setSelectedCategory] = useState(category);
 
-  const handleCategorySelect = (category) => () => {
+  const handleCategorySelect = (category: string) => () => {
     setSelectedCategory(category);
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
