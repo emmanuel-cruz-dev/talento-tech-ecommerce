cbcc206f6fc17912ab067c20c3d0a1ea6a285bb2
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-22 10:01:30 -0300

    feaT: adds CategoryId to src/constants/categoryIcons.tsx

--- src/constants/categoryIcons.tsx
+++ src/constants/categoryIcons.tsx
@@ -1,7 +1,12 @@
+import { ReactElement } from "react";
 import { Laptop, Gem, Icon } from "lucide-react";
 import { dress, trousers } from "@lucide/lab";
+import { CategoryId } from "../types/product.types";
 
-export const CATEGORY_ICONS = {
+export const CATEGORY_ICONS: Record<
+  Exclude<CategoryId, "all">,
+  ReactElement
+> = {
   electronics: <Laptop size={40} className="text-primary mb-2" />,
   jewelery: <Gem size={40} className="text-primary mb-2" />,
   "men's clothing": (
