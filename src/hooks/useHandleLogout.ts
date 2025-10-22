f9cca29e853b8f02f94aeceadac2b255903a587c
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-22 10:22:09 -0300

    fix: updates useAuth and useCart imports in src/hooks/useHandleLogout.ts

--- src/hooks/useHandleLogout.ts
+++ src/hooks/useHandleLogout.ts
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
-import { useCart } from "../context/CartContext";
+import { useAuth } from "./useAuth";
+import { useCart } from "./useCart";
 
 export const useHandleLogout = () => {
   const navigate = useNavigate();
