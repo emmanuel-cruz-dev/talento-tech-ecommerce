9ebebd67a9d5636ec9b30bc7bcbd132a25e0ab11
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-22 09:04:41 -0300

    feat: adds useAuth.ts to src/hooks/

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,12 @@
+import { createContext, useContext } from "react";
+import { AuthContextType } from "../types/auth.types";
+
+export const AuthContext = createContext<AuthContextType | null>(null);
+
+export const useAuth = (): AuthContextType => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within a AuthProvider");
+  }
+  return context;
+};
